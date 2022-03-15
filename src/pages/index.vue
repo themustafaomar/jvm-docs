a9e0d77cf978aa12b311de63b19e2c08a105f00f
Florian Gaa <15855432+bitbruder@users.noreply.github.com>
2022-03-15 10:16:09 +0100

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -15,7 +15,7 @@
         Discover now
       </router-link>
       <a href="https://github.com/themustafaomar/jsvectormap" type="button" class="py-2.5 px-4 mr-2 mb-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-        Visit Gibhub repo
+        Visit Github repo
       </a>
     </div>  
   </div>
