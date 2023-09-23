35aee36c57dc515ee9f2a287979ee841c049c9b6
찰스 <osairis100@gmail.com>
2023-09-23 15:19:52 +0800

    docs(typo): update main page (#8)

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,7 +9,7 @@
   <div class="max-w-4xl m-auto text-center mt-10 sm:mt-20">
     <vector-map id="main"></vector-map>
     <h1 class="text-3xl sm:text-4xl font-semibold text-center mb-3">Jsvectormap</h1>
-    <p class="max-w-3xl m-auto">Jsvectormap is a lightweight JavaScript library for creating interactive maps and pretty data visualization, supports all modern brownsers including IE9+.</p>
+    <p class="max-w-3xl m-auto">Jsvectormap is a lightweight JavaScript library for creating interactive maps and pretty data visualization, supports all modern browsers including IE9+.</p>
     <div class="mt-4">
       <router-link to="/docs/introduction" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-3 text-center mr-2 mb-2">
         Discover now
@@ -25,7 +25,7 @@
 import { useHead } from '@vueuse/head'
 import Navbar from '../components/Navbar.vue'
 
-const content = 'Jsvectormap is a lightweight JavaScript library for creating interactive maps and pretty data visualization, supports all modern brownsers including IE9+.'
+const content = 'Jsvectormap is a lightweight JavaScript library for creating interactive maps and pretty data visualization, supports all modern browsers including IE9+.'
 
 useHead({
   meta: [
